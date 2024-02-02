a9167b8803bc05319ef5ef71cd3fe79a42d30a4d
AngerOverApathy <violasian@gmail.com>
2024-02-01 16:21:05 -0800

    added state

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [todoList, setTodoList] = useState([])
+  const [currentTask, setCurrentTask] = useState('')
+
+  const addTask = () => {
+    if (currentTask !== '') {
+      setTodoList([...todoList, currentTask])
+      setCurrentTask('')
+    }
+  }
   
   return (
     <>
       <div className='App'>
         <h1>To Dos</h1>
         <div>
-          <input type='text' placeholder='Task' />
-          <button>Add Task</button>
+          <input type='text' placeholder='Task' onChange={(event) => {setCurrentTask(event.target.value)}}/>
+          <button onClick={}>Add Task</button>
         </div>
         <hr />
       </div>
